e3e80c7c2e800bc0e79dc9d690eec77d3ba9211a
avani <patelavani42@gmail.com>
2020-08-18 14:09:24 +0530

    packdemo added

---
+++ packdemo.java
@@ -0,0 +1,15 @@
+import pack1.*;
+class packdemo
+{
+	public static void main(String args[])
+	{
+		A a1=new A();
+		a1.displayA();
+
+		/*B b1=new B();
+		b1.displayB();*/
+
+		C c1=new C();
+		c1.displayC();
+	}
+}
